04eeb345bad41e1fda915f4dd4061ba025ca3817
Maxim Dovydyonok <m.dovydyonok@itransition.com>
2018-12-30 13:03:52 +0300

    Add crossplatform support

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -2,20 +2,57 @@ const Message = require('../model/Message')
 const ErrorType = require('../constants/errors')
 const MessageType = require('../constants/message-types')
 const {spawn} = require('child_process')
+const os = require('os')
+const commandExistsSync = require('command-exists').sync
+
+const useChromeUtils = commandExistsSync('/usr/local/bin/chrome-cli')
+
+const displayStatus = (code) => {
+  console.info(`Open command exited with code: ${code}`)
+}
+
+const openTab = async (url) => {
+  const platform = os.platform()
+
+  switch (platform) {
+    case 'darwin':
+      if (useChromeUtils) {
+        const chromeCli = spawn('/usr/local/bin/chrome-cli', ['open', url])
+
+        const id = +await new Promise((resolve) => {
+          chromeCli.stdout.on('data', resolve)
+        }).then(data => data.toString())
+          .then(output => output.match(/Id: (\d+)/)[1])
+
+        console.info(`Opened new tab: ${id}`)
+      } else {
+        spawn('open', [url]).on('close', displayStatus)
+      }
+
+      break
+    case 'linux':
+      spawn('xdg-open', [url]).on('close', displayStatus)
+
+      console.info('If tab haven\'t opened check that "xdg-open" util is working on your system')
+
+      break
+    case 'win32':
+      spawn('start', [url]).on('close', displayStatus)
+
+      console.info('If tab haven\'t opened check that "start" util is working on your system')
+
+      break
+    default:
+      console.error('Your platform is not supported')
+  }
+}
 
 const messageHandlers = {
   [MessageType.INIT]: () => {
     console.info('Connected')
   },
   [MessageType.OPEN]: async (socket, payload) => {
-    const chromeCli = spawn('/usr/local/bin/chrome-cli', ['open', payload.url])
-
-    const id = +await new Promise((resolve) => {
-      chromeCli.stdout.on('data', resolve)
-    }).then(data => data.toString())
-      .then(output => output.match(/Id: (\d+)/)[1])
-
-    console.info(`Opened new tab: ${id}`)
+    await openTab(payload.url)
   }
 }
 
